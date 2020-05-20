9c1a1a600ccb7d1300360763c44538075119a603
whatrye <>
2020-05-20 10:39:38 +0800

    mod

--- getTitleAndPic3.py
+++ getTitleAndPic3.py
@@ -1,10 +1,9 @@
 #! python3
 # -*- coding: UTF-8 -*-
 #coding: UTF-8
-#获取网页中的torrent的链接码,应用于jandown这个网站
+#获取网页中的图片链接和网页标题
 
 from bs4 import BeautifulSoup
-#import urllib2, urllib
 import requests
 
 def get_torrentTitleAndPic(myreq_url='https://bt.aisex.com/bt/htm_data/16/1609/860163.html',enable_proxy = False, proxy_string = {"http":"127.0.0.1:8787","https":"127.0.0.1:8787","socks":"127.0.0.1:1080"}):
@@ -18,12 +17,18 @@ def get_torrentTitleAndPic(myreq_url='https://bt.aisex.com/bt/htm_data/16/1609/8
 
     soup = BeautifulSoup(r1.content,'html.parser')
 
-    #获取img到列表imgs
+    #获取img链接到列表imgs
     imgs = []
     m_imgs = soup.find('div',id = 'read_tpc').find_all('img')
     for m_img in m_imgs:
         imgs.append(m_img.get('src'))
-    return imgs
+
+    #获取title
+    title = ''
+    title = soup.find('h1',id = 'subject_tpc').next_element #或者.get_text() 或者.text 或者.innerHTML 或者.innerText
+    title = title.strip()
+##    print(title)
+    return title,imgs
 
 if __name__ == '__main__':
-    print(get_torrentTitleAndPic(myreq_url='https://www.aisex.com/bt/htm_data/16/1609/860163.html'))
+    print(get_torrentTitleAndPic(myreq_url='https://bt.aisex.com/bt/htm_data/16/2005/1022016.html'))
